e98263fa36c05592887a1e604f69647a315d0c9f
Mael Jeuffrard <mj@ilatan.fr>
2021-04-11 03:20:40 +0200

    english

--- spamcop-tool.sh
+++ spamcop-tool.sh
@@ -20,7 +20,7 @@ function main(){
 
     if [ ! $(which nmap) ]; then
       echo ""
-      echo "  This option [-b] required nmap. Please installed it:"
+      echo "  This option [-b] required nmap. Please install it:"
       echo "    apt install nmap"
       echo ""
       exit 1
